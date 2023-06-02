d6840beb298e1ba0e0abe311e48d389c09f37217
APre203 <101435474+APre203@users.noreply.github.com>
2023-06-02 12:24:59 -0400

    Add files via upload
    Added my watched movies and python code. To make this code work, 2 csv files are required to be installed from "https://www.kaggle.com/datasets/grouplens/movielens-20m-dataset". These files are "movie.csv" and "rating.csv".

---
+++ data.py
@@ -0,0 +1,171 @@
+import requests
+import json
+import csv
+import re
+from unidecode import unidecode
+
+N = 50000 #number of users to base the movies on (the more users the better the results would be)
+def translate_csv(filename,n):
+  with open("rating.csv", "r") as f:
+    reader = csv.reader(f)
+    with open(filename, "w", newline= "") as file:
+      writer = csv.writer(file)
+      for lines in reader:
+        if lines[0] == "userId" or int(lines[0]) < n:
+          writer.writerow(lines)
+  file.close()
+  f.close()
+translate_csv("rating.csv",N)
+def get_movie(movie_name, year):  #returns movie id from the name
+
+  include_adult = "false"
+  url = f'https://api.themoviedb.org/3/search/movie?query={movie_name}&include_adult={include_adult}&language=en-US&page=1&year=' + str(year)
+
+  headers = {
+    "accept":
+    "application/json",
+    "Authorization":
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjZiYzYxMDZlZTE5NzU5MjBhOTQ1MjVlZTM0OTI4MCIsInN1YiI6IjY0NmQyYmMwNTRhMDk4MDE3MjhiYTk3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qKxT93gXNgcML9qOqhG1Di5Hn7dDvx4d4R4oKNAhzec"
+  }
+
+
+  response = requests.get(url, headers=headers)
+  response = response.text
+  
+  response = json.loads(response)
+  retval = []
+  if len(response["results"]) == 0:
+      url = f'https://api.themoviedb.org/3/search/movie?query={movie_name}&include_adult={include_adult}&language=en-US&page=1&year=""' 
+      response = requests.get(url, headers=headers)
+      response = response.text
+  
+      response = json.loads(response)
+      if len(response["results"]) == 0:
+        return []
+  
+  for r in response["results"]:
+    dic = {}
+    dic["year"] = r["release_date"][:4]
+    dic["title"] = r["title"]
+    dic["id"] = r["id"]
+    dic["genre"] = r["genre_ids"]
+    dic["popularity"] = r["popularity"]
+    dic["vote_average"] = r["vote_average"]
+    # dic["overview"] = r["overview"]
+    retval.append(dic)
+  if len(retval) != 1:
+    # print("More than 1 movie found, please include year or full title, or choose from the found movies below:")
+    return retval
+  return retval
+
+def get_actors(movie_id):  #returns list of actors from a movie
+
+  url = f'https://api.themoviedb.org/3/movie/{movie_id}/credits?language=en-US'
+
+  headers = {
+    "accept":
+    "application/json",
+    "Authorization":
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjZiYzYxMDZlZTE5NzU5MjBhOTQ1MjVlZTM0OTI4MCIsInN1YiI6IjY0NmQyYmMwNTRhMDk4MDE3MjhiYTk3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qKxT93gXNgcML9qOqhG1Di5Hn7dDvx4d4R4oKNAhzec"
+  }
+
+  response = requests.get(url, headers=headers)
+  response = response.text
+  response = json.loads(response)
+  retval = []
+
+  for p in response["cast"]:
+    dic = {}
+    dic["name"] = p["name"]
+    dic["id"] = p["id"]
+    dic["character"] = p["character"]
+    if p["gender"] == 2:
+      dic["gender"] = "Male"
+    else:
+      dic["gender"] = "Female"
+    dic["known_for_department"] = p["known_for_department"]
+    retval.append(dic)
+
+  return retval
+
+def get_genres(movie_id):
+  if movie_id == -1:
+    return []
+  url = f'https://api.themoviedb.org/3/movie/{movie_id}?language=en-US'
+
+  headers = {
+    "accept":
+    "application/json",
+    "Authorization":
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjZiYzYxMDZlZTE5NzU5MjBhOTQ1MjVlZTM0OTI4MCIsInN1YiI6IjY0NmQyYmMwNTRhMDk4MDE3MjhiYTk3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qKxT93gXNgcML9qOqhG1Di5Hn7dDvx4d4R4oKNAhzec"
+  }
+
+  response = requests.get(url, headers=headers)
+  response = response.text
+  response = json.loads(response)
+
+  retval = []
+  for g in response["genres"]:
+    dic = {}
+    dic["name"] = g["name"]
+    dic["id"] = g["id"]
+    retval.append(dic)
+  return retval
+
+def actor_movies(actor_id):  #returms id based off of a actors name
+
+  url = f'https://api.themoviedb.org/3/person/{actor_id}/movie_credits?language=en-US'
+  
+  headers = {
+      "accept": "application/json",
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjZiYzYxMDZlZTE5NzU5MjBhOTQ1MjVlZTM0OTI4MCIsInN1YiI6IjY0NmQyYmMwNTRhMDk4MDE3MjhiYTk3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qKxT93gXNgcML9qOqhG1Di5Hn7dDvx4d4R4oKNAhzec"
+  }
+  
+  response = requests.get(url, headers=headers)
+  response = response.text
+  response = json.loads(response)
+  retval = []
+  #cast, crew, id
+  for m in response["cast"]:
+    dic = {}
+    dic["title"] = m["title"]
+    dic["vote_average"] = m["vote_average"]
+    dic["genre_ids"] = m["genre_ids"]
+    dic["release_date"] = m["release_date"]
+    retval.append(dic)
+  return retval
+
+def all_genres():
+  
+  url = "https://api.themoviedb.org/3/genre/movie/list?language=en"
+  
+  headers = {
+      "accept": "application/json",
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjZiYzYxMDZlZTE5NzU5MjBhOTQ1MjVlZTM0OTI4MCIsInN1YiI6IjY0NmQyYmMwNTRhMDk4MDE3MjhiYTk3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qKxT93gXNgcML9qOqhG1Di5Hn7dDvx4d4R4oKNAhzec"
+  }
+  
+  response = requests.get(url, headers=headers)
+  response = response.text
+  response = json.loads(response)
+  return response["genres"]
+
+
+def clean(title):
+    new_title = title[:len(title)-7]
+    if ", The" in new_title:
+      new_title = new_title[:len(new_title)-5]
+      new_title = "The " + new_title
+    elif ", Les" in new_title:
+      new_title = new_title[:len(new_title)-5]
+      new_title = "Les " + new_title
+    if "(" in new_title:
+      for i in range(0,len(new_title)):
+        if new_title[i] == "(":
+          new_title = new_title[:i-1]
+          break
+    new_title = unidecode(new_title)
+    return re.sub("[^a-zA-Z0-9 ,]", "", new_title)
+
+
+#API Based on https://developer.themoviedb.org/reference/search-movie
+
---
+++ main.py
@@ -0,0 +1,116 @@
+from sklearn.decomposition import TruncatedSVD
+import pandas as pd
+import csv
+from data import clean, get_movie
+import re
+
+
+GENRE = "Action" #leave "" if no genre needed
+YEAR = "2010" #leave "" if no year needed
+
+
+def get_year(string):
+   mov_re = re.sub("[^a-zA-Z0-9 ,]", "", string)
+   year = mov_re[len(mov_re) - 4:]
+   return year
+
+def id_to_movie(movie_id, movie):
+   try:
+      mov = movie.loc[movie["movieId"] == movie_id, "title"].values[0]
+      year = get_year(mov)
+      mov = clean(mov) + " (" + year + ")" 
+      return mov
+   except:
+      # print(f"No movie found for {movie_id}")
+      return -1
+
+def movie_to_id(title, movie):
+   try:
+      retval = movie.loc[movie["title"].apply(clean) == clean(title), "movieId"].values[0] 
+      return retval
+   except:  
+      # print(f"No id found for {title}")
+      return -1
+
+def user_db(filename, movie): #creates a row for the user's movies and finds their movie id's in the movie.csv file
+   rating = []
+   title_id = []
+   with open(filename, "r") as f:
+      reader = csv.reader(f)
+      for lines in reader:
+         if lines[0] == "title":
+            pass
+         else:
+            dic = get_movie(lines[0],"")
+            if dic == []:
+               pass
+            else:
+               title = dic[0]["title"] + " (" + dic[0]["year"] + ")"
+
+               id = movie_to_id(title, movie)
+               if id == -1:
+                  pass
+               else:
+                  title_id.append(id)
+                  rating.append(lines[1])
+   
+   userId = [0]
+   
+   data = {col: rating for col, rating in zip(title_id, rating)}
+   u_matrix = pd.DataFrame(data, index=userId)
+
+   return u_matrix
+
+def rating(filename, genre, topN, ratings_df, movie, year):
+   user_id = 0
+   svd = TruncatedSVD(n_components=20)
+   
+   u_matrix = user_db(filename, movie)
+
+   ratings_matrix = ratings_df.pivot(index= "userId", columns= "movieId", values="rating")
+   ratings_matrix = pd.concat([u_matrix, ratings_matrix])
+
+   ratings_matrix = ratings_matrix.apply(pd.to_numeric, errors="coerce")
+   ratings_matrix = ratings_matrix[sorted(ratings_matrix.columns)]
+   ratings_matrix = ratings_matrix.fillna(ratings_matrix.mean())
+   
+   U = svd.fit_transform(ratings_matrix)
+   V = svd.components_
+   movie_list = []
+
+   for index in movie.T:
+
+      movie_id = int(movie["movieId"][index])
+      if movie_id in ratings_matrix.columns:
+         col_idx = ratings_matrix.columns.get_loc(movie_id)
+         if genre != "" or year != "" :
+            try:
+               if genre != "" and year == "":
+                  if genre in movie["genres"][index].split("|"):
+                     movie_list.append([movie_id, U[user_id] @ V[:, col_idx], id_to_movie(movie_id, movie)])
+               elif year != "" and genre == "":
+                  
+                  title = movie["title"][index]
+                  y = get_year(title)
+                  if int(y) >= int(year):
+                     movie_list.append([movie_id, U[user_id] @ V[:, col_idx], id_to_movie(movie_id, movie)])
+               else:
+                  title = movie["title"][index]
+                  y = get_year(title)
+                  if genre in movie["genres"][index].split("|") and int(y) >= int(year):
+                     movie_list.append([movie_id, U[user_id] @ V[:, col_idx], id_to_movie(movie_id, movie)])
+            except:
+               pass
+         else:
+            movie_list.append([movie_id, U[user_id] @ V[:, col_idx], id_to_movie(movie_id, movie)]) 
+   movie_list.sort(key = lambda x: x[1])
+   return movie_list[len(movie_list)-topN:]
+
+
+def main():
+   ratings_d = pd.read_csv("ratingsample.csv")
+   movi = pd.read_csv("movie.csv")
+   movie_list = rating("mymovie.csv",GENRE,5,ratings_d,movi,YEAR)
+   return movie_list
+
+print(main())
